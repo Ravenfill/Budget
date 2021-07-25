b480da0f353bcebe30d0bba16815e0e1063e771e
Ravenfill <auditoreven@gmail.com>
2021-07-25 17:37:13 +0300

    added database

--- app.py
+++ app.py
@@ -1,16 +1,44 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import defaultload
 from datetime import datetime
 
 app = Flask(__name__)
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database/database.db'
-#db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database/database.db'
+db = SQLAlchemy(app)
 
-@app.route('/')
+class Expences(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    category = db.Column(db.String(255), nullable=False)
+    product = db.Column(db.String(255), nullable=False)
+    price = db.Column(db.Float)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        item_category = request.form['category']
+        item_product = request.form['product']
+        item_price = request.form['price']
+        new_item = Expences(category=item_category, product=item_product, price=item_price)
 
+        try:
+            db.session.add(new_item)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "There was an issue adding your task"
+
+    else:
+        items = Expences.query.order_by(Expences.id).all()
+        return render_template(
+            'index.html',
+            categories=[{'name':'Продукты'}, {'name':'Развлечения'}],
+            items=items)
+
+@app.errorhandler(404)
+def page_not_fount(e):
+    return render_template('404.html'), 404
 
 if __name__ == "__main__":
     app.run(debug=True)
