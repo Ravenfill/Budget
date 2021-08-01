a506057b8399a7f6cc0b65e6116afaa26749d74c
Ravenfill <auditoreven@gmail.com>
2021-08-01 17:07:43 +0300

    heroku run

--- Budget-app/config.py
+++ Budget-app/config.py
@@ -1,9 +1,9 @@
 # Statement for enabling the development environment
-DEBUG = True
+DEBUG = False
 
 # Define the application directory
 import os
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))  
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 # Define the database - we are working with
 # SQLite for this example
@@ -20,7 +20,7 @@ THREADS_PER_PAGE = 2
 CSRF_ENABLED     = True
 
 # Use a secure, unique and absolutely secret key for
-# signing the data. 
+# signing the data.
 CSRF_SESSION_KEY = "secret"
 
 # Secret key for signing cookies
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: python /Budget-app/run.py
