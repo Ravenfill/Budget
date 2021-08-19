3d5ce7d94205c147420256dc3f878efdb0522614
Ravenfill <auditoreven@gmail.com>
2021-08-19 21:02:30 +0300

    Added placeholders in dashboard's form

--- app/dashboard/forms.py
+++ app/dashboard/forms.py
@@ -1,8 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SelectField, SubmitField, DecimalField
+from flask_babel import lazy_gettext
 
 class AddExpenceForm(FlaskForm):
     category_select = SelectField(choices=['Продукты', 'Развлечения', 'Налоги', 'Путешествия', 'Питомцы', 'Одежда', 'Транспорт', 'Медицина', 'Непредвиденные расходы',])
-    product_name = StringField('Продукт')
-    price_value = DecimalField('Цена')
+    product_name = StringField('Продукт', render_kw={"placeholder": lazy_gettext('Продукт')})
+    price_value = DecimalField('Цена', render_kw={"placeholder": lazy_gettext('Цена')})
     submit = SubmitField('Добавить')
