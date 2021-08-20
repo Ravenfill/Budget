0453eafd677394a84a38768bba802c75823baff5
VladSpiel <vlad.voronkoff.04@gmail.com>
2021-08-20 21:23:45 +0300

    Da

--- app/static/js/profileScript.js
+++ app/static/js/profileScript.js
@@ -1,6 +1,71 @@
 // Я удалил график
 
- 
+// За что график (((
+
+// Иконочки
+var food = "<img src='../../static/img/restaurant_black_24dp.svg'>";
+var enter = "<img src='../../static/img/sports_esports_black_24dp.svg'>";
+var pets = "<img src='../../static/img/pets_black_24dp.svg'>";
+var tax = "<img src='../../static/img/request_quote_black_24dp.svg'>";
+var travels = "<img src='../../static/img/map_black_24dp.svg'>";
+var clothes = "<img src='../../static/img/checkroom_black_24dp.svg'>";
+var transport = "<img src='../../static/img/commute_black_24dp.svg'>";
+var medicine = "<img src='../../static/img/local_hospital_black_24dp.svg'>";
+var other = "<img src='../../static/img/shopping_cart_black_24dp.svg'>";
+
+var len = $("td").filter(function() {return(this.id == "icon");}).length;
+
+for(var i = 0; i <= len; i++){
+    if($($(".icon")[i]).html() == '<div class="category"></div>FOOD'){
+        $($(".category")[i]).append(food);
+    }
+    else if($($(".icon")[i]).html() == '<div class="category"></div>ENTER'){
+        $($(".category")[i]).append(enter);
+    }
+    else if($($(".icon")[i]).html() == '<div class="category"></div>PETS'){
+        $($(".category")[i]).append(pets);
+    }
+    else if($($(".icon")[i]).html() == '<div class="category"></div>TAX'){
+        $($(".category")[i]).append(tax);
+    }
+    else if($($(".icon")[i]).html() == '<div class="category"></div>TRAVELS'){
+        $($(".category")[i]).append(travels);
+    }
+    else if($($(".icon")[i]).html() == '<div class="category"></div>CLOTHES'){
+        $($(".category")[i]).append(clothes);
+    }
+    else if($($(".icon")[i]).html() == '<div class="category"></div>TRANS'){
+        $($(".category")[i]).append(transport);
+    }
+    else if($($(".icon")[i]).html() == '<div class="category"></div>MEDICINE'){
+        $($(".category")[i]).append(medicine);
+    }
+    else{
+        $($(".category")[i]).append(other);
+    }
+}
+function resize(){
+    // Эта шляпа короче выравнивает левую колонку что бы не было пустот
+    var num_of_tables = $("tr").filter(function() {return(this.id == "num_of_tables");}).length;
+
+    // len - кол-во элементов в таблицах
+    // num_of_tables - кол-во таблиц
+
+    if(window.screen.height < 1000 && len >= 3 && num_of_tables > 1){
+        document.getElementById("left-column").style.height = "auto";
+    }
+    else{
+        document.getElementById("left-column").style.height = "100vh";
+    }
+}
+resize();
+
+window.addEventListener('resize', change_screen_size);
+function change_screen_size(){
+    resize();
+}
+
+
 if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
     document.getElementById("add-purchase").value = "+ добавить покупку";
 }
@@ -12,6 +77,9 @@ else{
     function on_mouse_exit(){
         document.getElementById("add-purchase").value = "+";
     }
+    
+
+
 }
 
 
